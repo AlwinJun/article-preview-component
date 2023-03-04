c4b925aea163125de489da143eded5444813905e
AJ <mahidlawonaljun@gmail.com>
2023-03-04 16:00:41 +0800

    Add click event to show active share

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('test')
+// console.log('test')
+
+const shareButton = document.querySelector(".share-inactive img[alt='share-button']")
+
+shareButton.addEventListener('click',() => {
+  document.querySelector('.share-acive').classList.remove('hidden');
+  document.querySelector('.share-inactive').classList.add('hidden')
+})
