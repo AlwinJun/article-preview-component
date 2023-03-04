e3c1ca7a76ecfcf4c9d189dadc5e8f7564a66ebb
AJ <mahidlawonaljun@gmail.com>
2023-03-04 17:22:43 +0800

    Change click event behavior base on device screen size

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@
 const shareButton = document.querySelector(".share-inactive img[alt='share-button']")
 
 shareButton.addEventListener('click',() => {
-  document.querySelector('.share-acive').classList.remove('hidden');
-  document.querySelector('.share-inactive').classList.add('hidden')
+  const activeShare = document.querySelector('.share-acive');
+  const activeShareButton = document.querySelector(".share-acive img[alt='share-button']")
+  const mediaScreen = window.matchMedia('(min-width: 60.125em)');
+
+  if(mediaScreen.matches){
+    activeShare.classList.remove('hidden');
+
+    if(activeShare.contains(activeShareButton)){
+      activeShare.removeChild(activeShareButton)
+    }
+  }else{
+    activeShare.classList.remove('hidden');
+    document.querySelector('.share-inactive').classList.add('hidden');
+  }
+
+
+ 
 })
